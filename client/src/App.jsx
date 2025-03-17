4dac597b1858674adfd2e6763f035c4e6594d9da
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-03-17 17:19:40 +0700

    [MS-2][Client] Setup pages router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,32 @@
+import { Route, Routes } from 'react-router-dom';
+import {
+  About,
+  Appointment,
+  Contact,
+  Doctors,
+  Home,
+  Login,
+  MyAppointments,
+  MyProfile,
+} from './pages';
+import { Footer, Header } from './components';
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl">Hello world!</h1>
-    </>
+    <div className="mx-4 sm:mx-[10%]">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/my-appointments" element={<MyAppointments />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/appointment/:doctorId" element={<Appointment />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
---
+++ client/src/pages/index.js
@@ -0,0 +1,9 @@
+export { default as Home } from "./Home.jsx";
+export { default as About } from "./About.jsx";
+export { default as Contact } from "./Contact.jsx";
+export { default as Doctors } from "./Doctors.jsx";
+export { default as Login } from "./Login.jsx";
+export { default as MyAppointments } from "./MyAppointments.jsx";
+export { default as MyProfile } from "./MyProfile.jsx";
+export { default as Appointment } from "./Appointment.jsx";
+
