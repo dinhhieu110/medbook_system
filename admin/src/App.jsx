23a3a2ca4a8d5030c84b1730402c0888bbd3de2d
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-03-23 15:43:26 +0700

    [MS-27][Client] Build admin navbar component

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,14 +1,16 @@
 import React, { useContext } from "react";
 import Login from "./pages/Login";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import { AdminContext } from "./context/AdminContext";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   const { adminToken } = useContext(AdminContext);
 
   return adminToken ? (
-    <div>
+    <div className="bg-[#F8F9FD]">
       <ToastContainer />
+      <Navbar />
     </div>
   ) : (
     <>
